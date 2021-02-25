fc2ff73e2ccbe035d35422dec647bf1f1255c1aa
Gavin Tynan <35689862+gtynan@users.noreply.github.com>
2021-02-25 16:59:11 +0000

    Update config.py

--- src/core/config.py
+++ src/core/config.py
@@ -1,8 +1,9 @@
 from starlette.config import Config
+import os
 
 config = Config(".env")
 
 # address of Google matrix API
 API_ADDRESS = "https://maps.googleapis.com/maps/api/distancematrix/json"
 # google maps API KEY (used to create distance matrix for real locations)
-API_KEY = config("API_KEY", cast=str)
+API_KEY = os.environ['API_KEY'] #config("API_KEY", cast=str)
