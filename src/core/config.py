98fb6928de03825ced4b0cc5a7e26490fb5f49fc
Gavin Tynan <35689862+gtynan@users.noreply.github.com>
2021-02-26 09:18:04 +0000

    moved secret to gcloud

--- src/core/config.py
+++ src/core/config.py
@@ -1,9 +1,8 @@
 from starlette.config import Config
-import os
 
 config = Config(".env")
 
 # address of Google matrix API
 API_ADDRESS = "https://maps.googleapis.com/maps/api/distancematrix/json"
 # google maps API KEY (used to create distance matrix for real locations)
-API_KEY = os.environ['API_KEY'] #config("API_KEY", cast=str)
+API_KEY = config("API_KEY", cast=str)
